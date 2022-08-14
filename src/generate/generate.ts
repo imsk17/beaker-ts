c1e3a76cd18ac5950106c3e96fd3d2d7eb842efd
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-14 10:56:38 -0400

    generating file

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -1,63 +1,60 @@
-import {AppSpec} from './appspec'
+import { AppSpec } from "./appspec";
 
 import ts, { factory } from "typescript";
 import { writeFileSync } from "fs";
 
-
-function generateStruct(): ts.NodeArray<ts.Node>{
-    // create name property
-    const nameProp = factory.createPropertySignature(
+function generateStruct(): ts.NodeArray<ts.Node> {
+  // create name property
+  const nameProp = factory.createPropertySignature(
     undefined,
     factory.createIdentifier("name"),
     undefined,
     factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword)
-    );
+  );
 
-    // create age property
-    const ageProp = factory.createPropertySignature(
+  // create age property
+  const ageProp = factory.createPropertySignature(
     undefined,
     factory.createIdentifier("age"),
     undefined,
     factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword)
-    );
+  );
 
-    // create User type
-    const userType = factory.createTypeAliasDeclaration(
-        undefined,
-        [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
-        factory.createIdentifier("User"),
-        undefined,
-        factory.createTypeLiteralNode([nameProp, ageProp])
-    );
+  // create User type
+  const userType = factory.createTypeAliasDeclaration(
+    undefined,
+    [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+    factory.createIdentifier("User"),
+    undefined,
+    factory.createTypeLiteralNode([nameProp, ageProp])
+  );
 
-    const nodes = factory.createNodeArray([userType]);
-    return nodes
+  const nodes = factory.createNodeArray([userType]);
+  return nodes;
 }
 
-
 export function generateClient(appSpec: AppSpec, path: string) {
-    console.log(appSpec)
+  console.log(appSpec);
 
-    const name = `${appSpec.contract.name}_client.ts`
-    const sourceFile = ts.createSourceFile(
-        name,
-        "",
-        ts.ScriptTarget.ESNext,
-        true,
-        ts.ScriptKind.TS
-    );
+  const name = `${appSpec.contract.name}_client.ts`;
+  const sourceFile = ts.createSourceFile(
+    name,
+    "",
+    ts.ScriptTarget.ESNext,
+    true,
+    ts.ScriptKind.TS
+  );
 
-    const printer = ts.createPrinter();
+  const printer = ts.createPrinter();
 
-    const nodes = generateStruct()
-    const outputFile = printer.printList(
-        ts.ListFormat.MultiLine,
-        nodes,
-        sourceFile
-    );
+  const nodes = generateStruct();
+  const outputFile = printer.printList(
+    ts.ListFormat.MultiLine,
+    nodes,
+    sourceFile
+  );
 
+  //... other code from above
 
-    //... other code from above
-
-    writeFileSync(path+name, outputFile);
+  writeFileSync(path + name, outputFile);
 }
