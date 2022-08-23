30b0139ed3910843a134f060dad4bdf74a238024
Ernesto Speranza <ernestosperanza@gmail.com>
2022-08-23 11:39:16 -0300

    Add type Byte and Enums, edits on the readme

--- src/generate/appspec.ts
+++ src/generate/appspec.ts
@@ -1,70 +1,71 @@
-import algosdk from 'algosdk';
+import algosdk from "algosdk";
 
+export interface HintSpec {}
 
-export interface HintSpec { }
-
-export interface DeclaredSchemaValueSpec { 
-    type: string // TODO: should be enum of uint64|bytes
-    key: string // TODO: should be bytes
-    desc: string
-    static: boolean
+export interface DeclaredSchemaValueSpec {
+  type: Type;
+  key: Bytes;
+  desc: string;
+  static: boolean;
 }
 
-export interface DynamicSchemaValueSpec { 
-    type: string // TODO: should be enum of uint64|bytes
-    desc: string
-    maxKeys: number
+export interface DynamicSchemaValueSpec {
+  type: Type;
+  desc: string;
+  maxKeys: number;
 }
 
 export interface Schema {
-    declared: {
-        [key: string]: DeclaredSchemaValueSpec
-    },
-    dynamic: {
-        [key: string]: DynamicSchemaValueSpec
-    }
+  declared: {
+    [key: string]: DeclaredSchemaValueSpec;
+  };
+  dynamic: {
+    [key: string]: DynamicSchemaValueSpec;
+  };
 }
 
 export type StateSchema = {
-    uints: number
-    bytes: number
-}
+  uints: number;
+  bytes: number;
+};
 
 export function getStateSchema(s: Schema): StateSchema {
-    let uints = 0
-    let bytes = 0
+  let uints = 0;
+  let bytes = 0;
 
-    for(const item of Object.entries(s.declared)){
-        if(item[1].type == "bytes")
-            bytes += 1
-        if(item[1].type == "uint64")
-            uints += 1
-    }
+  for (const item of Object.entries(s.declared)) {
+    if (item[1].type == "bytes") bytes += 1;
+    if (item[1].type == "uint64") uints += 1;
+  }
 
-    for(const item of Object.entries(s.dynamic)){
-        if(item[1].type == "bytes")
-            bytes += item[1].maxKeys
-        if(item[1].type == "uint64")
-            uints += item[1].maxKeys
-    }
+  for (const item of Object.entries(s.dynamic)) {
+    if (item[1].type == "bytes") bytes += item[1].maxKeys;
+    if (item[1].type == "uint64") uints += item[1].maxKeys;
+  }
 
-    return { uints: uints, bytes: bytes }
+  return { uints: uints, bytes: bytes };
 }
 
-
 export interface SchemaSpec {
-    local: Schema 
-    global: Schema 
+  local: Schema;
+  global: Schema;
 }
 
 export interface AppSources {
-    approval: string
-    clear: string
+  approval: string;
+  clear: string;
 }
 
 export interface AppSpec {
-    hints: HintSpec
-    schema: SchemaSpec
-    source: AppSources
-    contract: algosdk.ABIContract
+  hints: HintSpec;
+  schema: SchemaSpec;
+  source: AppSources;
+  contract: algosdk.ABIContract;
 }
+
+export enum Type {
+  uint64,
+  bytes,
+}
+
+export type Bytes = string;
