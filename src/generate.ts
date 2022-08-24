a37f1312d63e0a756beec66fc85620d466b27ca0
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-24 15:44:53 -0400

    stuff

---
+++ src/generate.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env ts-node
+
+import { generateClient } from "./generate/generate";
+import { AppSpec } from "./generate/appspec";
+import fs from "fs";
+
+
+// TODO: make this friendly
+// Currently `npm run gen $spec_path_to_read $path_to_write_generated_client
+const args = process.argv
+const path = args[args.length-1]
+const spec = args[args.length-2]
+
+generateClient(
+  JSON.parse(fs.readFileSync(spec).toString()) as AppSpec,
+  path 
+);
