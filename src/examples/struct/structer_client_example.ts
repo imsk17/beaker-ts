eb26cb52f999ceefce38a4dcf110a45c1a9dd06b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-27 07:28:54 -0400

    rm example thing

--- src/examples/struct/structer_client_example.ts
+++ src/examples/struct/structer_client_example.ts
@@ -19,6 +19,5 @@ import {Order, Structer} from "./structer_client";
   console.log(result.txID); 
 
   const result2 = await appClient.increase_quantity(1);
-  result2.value
   console.log("Result: ", result2.value); 
 })();
