d0abd915d84c165982dd588519b404c505ad6e72
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-01 13:17:47 -0400

    adding index

---
+++ src/examples/opup/index.ts
@@ -0,0 +1,32 @@
+import algosdk from "algosdk";
+import { getAccounts, getAlgodClient } from "../../";
+import { ExpensiveApp } from "./expensiveapp_client";
+
+(async function () {
+  const acct = (await getAccounts()).pop();
+
+  const appClient = new ExpensiveApp({
+    client: getAlgodClient(),
+    signer: acct.signer,
+    sender: acct.addr,
+  });
+
+  const [appId, appAddr, txId] = await appClient.create();
+  console.log(`Created app ${appId} with address ${appAddr} in tx ${txId}`);
+
+  const sp = await appClient.getSuggestedParams();
+  sp.flatFee = true;
+  sp.fee = 1e6;
+
+  await appClient.opup_bootstrap({
+    ptxn: algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: acct.addr,
+      suggestedParams: sp,
+      to: appAddr,
+      amount: BigInt(1e6),
+    }),
+  });
+
+  const result = await appClient.hash_it({ input: "asdf", iters: BigInt(100) }, {suggestedParams:sp});
+  console.log(result.returnValue);
+})();
---
+++ src/examples/struct/index.ts
@@ -0,0 +1,39 @@
+import { isUint8Array } from "util/types";
+import { getAccounts, getAlgodClient } from "../../";
+import { Order, Structer } from "./structer_client";
+
+(async function () {
+  const acct = (await getAccounts()).pop();
+
+  const appClient = new Structer({
+    client: getAlgodClient(),
+    signer: acct.signer,
+    sender: acct.addr,
+  });
+
+  const [appId, appAddr, txId] = await appClient.create();
+  console.log(`Created app ${appId} with address ${appAddr} in tx ${txId}`);
+
+  await appClient.optIn();
+
+  const result = await appClient.place_order({
+    order_number: BigInt(1),
+    order: { item: "cubes", quantity: BigInt(1) },
+  });
+  console.log(result.txID);
+
+  const result2 = await appClient.increase_quantity({
+    order_number: BigInt(1),
+  });
+  console.log("Result: ", result2.value);
+  console.log("Or: ", Order.decodeResult(result2.returnValue));
+
+  const state = await appClient.getAccountState(acct.addr, true);
+  for (const k in state) {
+    // appease ts
+    const val = state[k];
+    if (!isUint8Array(val)) continue;
+
+    console.log(Order.decodeBytes(val));
+  }
+})();
