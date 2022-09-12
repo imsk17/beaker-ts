bb45b30e3983fd44ba4f9a04b578045935ccfc5f
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-12 12:26:30 -0400

    bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ApplicationClient, ABIResult, MethodArg, MethodArgs, decodeNamedTuple, TransactionOverrides }  from "./application_client/application_client";
-export { LogicError } from "./application_client/logic_error";
+export { LogicError, parseLogicError } from "./application_client/logic_error";
 
 export {generateApplicationClient} from "./generate/generate";
 export { HintSpec, DeclaredSchemaValueSpec, DynamicSchemaValueSpec, Schema, StateSchema, SchemaSpec, AppSources, AppSpec, AVMType, getStateSchema } from "./generate/appspec";
