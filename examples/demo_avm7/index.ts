ccc16711d45e1aef0ca7c54b85a27e75e1135459
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-19 09:17:55 -0400

    commentsg

--- examples/demo_avm7/index.ts
+++ examples/demo_avm7/index.ts
@@ -1,12 +1,12 @@
-import algosdk, { AtomicTransactionComposer } from 'algosdk'
-import nacl from 'tweetnacl';
+import algosdk, { AtomicTransactionComposer } from "algosdk";
+import nacl from "tweetnacl";
 import * as bkr from "../../src";
 import { DemoAVM7 } from "./demoavm7_client";
 
 (async function () {
   const acct = (await bkr.sandbox.getAccounts()).pop();
 
-  if(acct === undefined) return
+  if (acct === undefined) return;
 
   const appClient = new DemoAVM7({
     client: bkr.sandbox.getAlgodClient(),
@@ -17,38 +17,56 @@ import { DemoAVM7 } from "./demoavm7_client";
   const [appId, appAddr, txId] = await appClient.create();
   console.log(`Created app ${appId} with address ${appAddr} in tx ${txId}`);
 
-   const json_ref_result = await appClient.json_ref({
-     json_str: JSON.stringify({
-       string_key: "In Xanadu did Kubla Khan",
-       uint_key: 42,
-       obj_key: { lol: "lmao" },
-     }),
-   });
-   console.log(json_ref_result.value);
+  // 
+  // Use JSON ref opcodes
+  // 
+  const json_ref_result = await appClient.json_ref({
+    json_str: JSON.stringify({
+      string_key: "In Xanadu did Kubla Khan",
+      uint_key: 42,
+      obj_key: { lol: "lmao" },
+    }),
+  });
+  console.log(json_ref_result.value);
 
-   const sp = await appClient.client.getTransactionParams().do();
-   const block_result = await appClient.block({
-     round: BigInt(sp.firstRound - 1),
-   });
-   console.log(block_result.value);
+  //
+  // Get block params
+  //
+  const sp = await appClient.client.getTransactionParams().do();
+  const block_result = await appClient.block({
+    round: BigInt(sp.firstRound - 1),
+  });
+  console.log(block_result.value);
 
-    function bytesigner(sk: Uint8Array, msg: string): Uint8Array {
-      const sig =  nacl.sign.detached(Buffer.from(msg), sk);
-      return new Uint8Array(sig.buffer)
-    }
+  //
+  // ED25519 Verify Bare
+  // 
+  function bytesigner(sk: Uint8Array, msg: string): Uint8Array {
+    const sig = nacl.sign.detached(Buffer.from(msg), sk);
+    return new Uint8Array(sig.buffer);
+  }
 
-    const message = "Sign me please"
-    const sig = bytesigner(new Uint8Array(acct.privateKey), message)
+  const message = "Sign me please";
+  const sig = bytesigner(new Uint8Array(acct.privateKey), message);
 
-    const verifyMethod = algosdk.getMethodByName(appClient.methods, "ed25519verify_bare")
-    const noopMethod = algosdk.getMethodByName(appClient.methods, "noop")
+  const verifyMethod = algosdk.getMethodByName(
+    appClient.methods,
+    "ed25519verify_bare"
+  );
+  const noopMethod = algosdk.getMethodByName(appClient.methods, "noop");
 
-    let atc = new AtomicTransactionComposer()
-    await appClient.addMethodCall(atc, verifyMethod, {msg: message, sig: sig})
-    await appClient.addMethodCall(atc, noopMethod, undefined, {note:new Uint8Array(Buffer.from("noncey1"))})
-    await appClient.addMethodCall(atc, noopMethod, undefined, {note:new Uint8Array(Buffer.from("noncey2"))})
+  let atc = new AtomicTransactionComposer();
+  await appClient.addMethodCall(atc, verifyMethod, { msg: message, sig: sig });
+  // Add noop calls to increase our opcode budget since ed25519 is expensive
+  await appClient.addMethodCall(atc, noopMethod, undefined, {
+    note: new Uint8Array(Buffer.from("noncey1")),
+  });
+  await appClient.addMethodCall(atc, noopMethod, undefined, {
+    note: new Uint8Array(Buffer.from("noncey2")),
+  });
 
-    const result = await atc.execute(appClient.client, 4)
-    console.log(result.methodResults)
+  const result = await atc.execute(appClient.client, 4);
+  console.log(result.methodResults);
+
 
 })();
