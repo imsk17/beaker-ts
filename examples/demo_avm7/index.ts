d3afc497165b931db7eb99e6e2065c2242119f51
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-09-19 09:16:31 -0400

    restore other calls

--- examples/demo_avm7/index.ts
+++ examples/demo_avm7/index.ts
@@ -17,20 +17,20 @@ import { DemoAVM7 } from "./demoavm7_client";
   const [appId, appAddr, txId] = await appClient.create();
   console.log(`Created app ${appId} with address ${appAddr} in tx ${txId}`);
 
-  // const json_ref_result = await appClient.json_ref({
-  //   json_str: JSON.stringify({
-  //     string_key: "In Xanadu did Kubla Khan",
-  //     uint_key: 42,
-  //     obj_key: { lol: "lmao" },
-  //   }),
-  // });
-  // console.log(json_ref_result.value);
+   const json_ref_result = await appClient.json_ref({
+     json_str: JSON.stringify({
+       string_key: "In Xanadu did Kubla Khan",
+       uint_key: 42,
+       obj_key: { lol: "lmao" },
+     }),
+   });
+   console.log(json_ref_result.value);
 
-  // const sp = await appClient.client.getTransactionParams().do();
-  // const block_result = await appClient.block({
-  //   round: BigInt(sp.firstRound - 1),
-  // });
-  // console.log(block_result.value);
+   const sp = await appClient.client.getTransactionParams().do();
+   const block_result = await appClient.block({
+     round: BigInt(sp.firstRound - 1),
+   });
+   console.log(block_result.value);
 
     function bytesigner(sk: Uint8Array, msg: string): Uint8Array {
       const sig =  nacl.sign.detached(Buffer.from(msg), sk);
